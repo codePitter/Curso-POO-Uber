fde7949a45d6bf672cd36da2c65d78803f07d101
Pitter <pitterbck@gmail.com>
2021-08-19 04:04:49 -0300

    Actualización

--- PHP/index.php
+++ PHP/index.php
@@ -1,8 +1,9 @@
 <?php
 
-require_once('Car.php');
-require_once('Account.php');
+require_once('car.php');
+require_once('uberX.php');
+require_once('account.php');
 
-$car = new Car("AW456", new Account("Andres Herrera", "AMS123"));
-$car->printDataCar();
+$uberX = new UberX("CVB123", new Account("Andres Herrera", "AND654"), "Chevrolet", "Spark");
+$uberX->printDAtaCar();
 ?>
